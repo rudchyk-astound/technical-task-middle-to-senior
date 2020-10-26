fa30161ac209116e2abce4bb0a24a961c6a08b9d
S.Rudchyk (Astoundcommerce) <s.rudchyk@astoundcommerce.com>
2020-10-26 13:46:37 +0200

    first init

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  watch: true,
+  devServer: {
+    hot: true,
+    compress: true,
+    contentBase: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    port: 9000,
+    before: function(app, server, compiler) {
+      app.get('/some/path', function(req, res) {
+        res.json({ custom: 'response' });
+      });
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html')
+    }),
+  ],
+};
