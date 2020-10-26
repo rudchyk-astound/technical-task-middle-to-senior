e0a7fe0f3594e7d794957064e065e1a4f4291d46
S.Rudchyk (Astoundcommerce) <s.rudchyk@astoundcommerce.com>
2020-10-26 15:21:56 +0200

    fixed watch prop

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  watch: true,
   devServer: {
-    hot: true,
     compress: true,
     contentBase: path.join(__dirname, 'dist'),
     publicPath: '/',
